dcb6c66a70b3ffd21f5d2bcc4c286d133e535703
Giovanni Urbina <giovanni.urbina23@gmail.com>
2024-12-23 00:31:13 -0600

    testssh

--- server.js
+++ server.js
@@ -16,9 +16,13 @@ app.use("/api/ordenes", verificarToken, require("./routes/ordenes"));
 
 // Rutas existentes
 app.use("/api/productos", require("./routes/productos"));
+//ruta categorias
 app.use("/api/categorias", require("./routes/categorias"));
+//ruta usuarios
 app.use("/api/usuarios", require("./routes/usuarios"));
+//ruta ordenes
 app.use("/api/ordenes", require("./routes/ordenes"));
+//ruta clientes
 app.use("/api/clientes", require("./routes/clientes"));
 
 // Probar la conexión a la base de datos
